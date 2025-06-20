f6484a69850674b633a435d2afc3d89bfad4bf70
Nitish Choudhary <142423896+Nitish1234556@users.noreply.github.com>
2025-06-20 08:10:43 +0530

    code added

---
+++ delivery.cpp
@@ -0,0 +1,28 @@
+# include "headers.h"
+
+vector<Courier>couriers;
+
+
+int priority_value(const string& p){
+   if(p == "high") return 3;
+   if(p == "medium") return 2;
+   if(p == "low") return 1;
+   return 0;
+}
+
+bool compare_priority(const Courier& a , const Courier& b){
+    return a.priority_value > b.priority_value;
+}
+
+void read_courier(const string& filename){
+    ifstream infile(filename);
+
+    int dest;
+    string prio;
+
+    while(infile>>dest>>prio){
+        couriers.push_back({dest,prio,priority_value(prio)});
+    }
+
+    sort(couriers.begin(), couriers.end(), compare_priority);
+}
